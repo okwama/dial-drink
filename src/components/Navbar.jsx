731f5a307b0aafec6f62f568bfb53a472e64038d
Benjamin Okwama <bennjiokwama@gmailcom>
2024-04-20 00:02:44 +0300

    amended cart && mobile responsive js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import { Badge } from '@material-ui/core';
 import { Search, ShoppingCartOutlined } from '@material-ui/icons';
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import {mobile} from "../responsive"
+
 
 const Container = styled.div`
 height: 60px;
-
-`
+${mobile({ height: "50px" })}
+`;
 const Wrapper = styled.div`
 padding: 10px 20px;
 display: flex;
 align-items: center;
 justify-content: space-between;
+${mobile({ padding: "10px 0px" })}
 `
 const Left = styled.div`
 flex:1;
@@ -22,18 +25,20 @@ align-items: center;
 const Language = styled.span`
 font-size: 14px;
 cursor: pointer;
+${mobile({ display: "none" })}
 `
 const SearchContainer = styled.div`
 border: 0.5px solid lightgray;
 display: flex;
 align-items: center;
-margin-left: 25px;
+margin-left: 28px;
 padding: 5px;
 
 `
 const Input = styled.input`
 border: none;
 flex:8;
+${mobile({ Width: "50px" })}
 `;
 
 const Center = styled.div`
@@ -50,12 +55,14 @@ flex: 1;
 display: flex;
 align-items: center;
 justify-content: flex-end;
+${mobile({ flex: 2, justifyContent: "center" })}
 `;
 
 const MenuItem = styled.div`
 font-size: 14px;
 cursor:pointer;
 margin-left: 25px;
+${mobile({ fontSize: "12px" , marginLeft:"10px" })}
 `
 const Navbar = () => {
   return (
